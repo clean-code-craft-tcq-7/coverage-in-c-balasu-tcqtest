8bb763ce9393d3a4b4daa67fe374e0927e771f82
APAC\bki8cob <balasubramanian.krishnamoorthy@in.bosch.com>
2023-01-09 18:28:42 +0530

    Added test for func inferBreach

--- test_breach_analyzer.cpp
+++ test_breach_analyzer.cpp
@@ -4,6 +4,22 @@
 #include "product_config.h"
 #include "breach_analyzer.h"
 
-TEST_CASE("infers the breach according to limits") {
+TEST_CASE("Value less than lower limit") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+}
+
+TEST_CASE("Value between the limit") {
+  REQUIRE(inferBreach(22, 20, 30) == NORMAL);
+}
+
+TEST_CASE("Value greater than the upper limit") {
+  REQUIRE(inferBreach(22, 20, 30) == TOO_HIGH);
+}
+
+TEST_CASE("Value equal to lower limit") {
+  REQUIRE(inferBreach(20, 20, 30) == NORMAL);
+}
+
+TEST_CASE("Value equal to upper limit") {
+  REQUIRE(inferBreach(30, 20, 30) == NORMAL);
 }
