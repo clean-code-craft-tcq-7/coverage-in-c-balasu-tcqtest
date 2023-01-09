7d6e6c6bb5c8496d346df520fa595fa0dec79c66
APAC\bki8cob <balasubramanian.krishnamoorthy@in.bosch.com>
2023-01-09 17:28:41 +0530

    Corrected Build Error1

--- breach_analyzer.cpp
+++ breach_analyzer.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "test/catch.hpp"
+#include "product_config.h"
 #include "breach_analyzer.h"
 
 TEST_CASE("infers the breach according to limits") {
--- breach_analyzer.h
+++ breach_analyzer.h
@@ -1,6 +1,4 @@
 #pragma once
 
-
-
 BreachType inferBreach(double value, double lowerLimit, double upperLimit);
 BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
