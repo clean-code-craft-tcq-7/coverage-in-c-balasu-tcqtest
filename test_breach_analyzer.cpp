97dc096f736809ddfce494feccf40d9ed809308e
APAC\bki8cob <balasubramanian.krishnamoorthy@in.bosch.com>
2023-01-09 18:49:27 +0530

    Added test for func classifyTemperatureBreach

--- test_breach_analyzer.cpp
+++ test_breach_analyzer.cpp
@@ -4,22 +4,26 @@
 #include "product_config.h"
 #include "breach_analyzer.h"
 
-TEST_CASE("Value less than lower limit") {
+TEST_CASE("Value less than lower limit", "[inferBreach]") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
 }
 
-TEST_CASE("Value between the limit") {
+TEST_CASE("Value between the limit", "[inferBreach]") {
   REQUIRE(inferBreach(22, 20, 30) == NORMAL);
 }
 
-TEST_CASE("Value greater than the upper limit") {
+TEST_CASE("Value greater than the upper limit", "[inferBreach]") {
   REQUIRE(inferBreach(32, 20, 30) == TOO_HIGH);
 }
 
-TEST_CASE("Value equal to lower limit") {
+TEST_CASE("Value equal to lower limit", "[inferBreach]") {
   REQUIRE(inferBreach(20, 20, 30) == NORMAL);
 }
 
-TEST_CASE("Value equal to upper limit") {
+TEST_CASE("Value equal to upper limit", "[inferBreach]") {
   REQUIRE(inferBreach(30, 20, 30) == NORMAL);
+}
+
+TEST_CASE("Check ouptut of interface function", ["classifyTemperatureBreach"]){
+    REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 20) == NORMAL);
 }
