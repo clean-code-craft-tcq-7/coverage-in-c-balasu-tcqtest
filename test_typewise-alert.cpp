480eb70ef8bbfe2a5642a137a1937987255f19c5
APAC\bki8cob <balasubramanian.krishnamoorthy@in.bosch.com>
2023-01-10 12:02:03 +0530

    Added test case for typewise-alert.c

--- test_typewise-alert.cpp
+++ test_typewise-alert.cpp
@@ -4,11 +4,11 @@
 #include "product_config.h"
 #include "typewise-alert.h"
 
-char printStr[100];
+char printStr1[100];
 
 void printfunc(char* buff)
 {
-    strcpy(printStr, buff);
+    strcpy(printStr1, buff);
 }
 
 TEST_CASE("Alert via Email HI_ACTIVE_COOLING high breach"){
@@ -18,5 +18,5 @@ TEST_CASE("Alert via Email HI_ACTIVE_COOLING high breach"){
 
     checkAndAlert(TO_EMAIL, batteryChar, 46, &printfunc);
 
-    REQUIRE(strcmp(printStr,"To: a.b@c.com\nHi, the temperature is too high\n") == 0);
+    REQUIRE(strcmp(printStr1,"To: a.b@c.com\nHi, the temperature is too high\n") == 0);
 }
