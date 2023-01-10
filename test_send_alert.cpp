cb71757450986dc2e58f11de69702d58a9d87c18
APAC\bki8cob <balasubramanian.krishnamoorthy@in.bosch.com>
2023-01-10 10:44:13 +0530

    Added test file for send_alert.c

--- test_send_alert.cpp
+++ test_send_alert.cpp
@@ -15,3 +15,15 @@ TEST_CASE("Alert Controller Too Low Breach"){
     alertTypeList[TO_CONTROLLER](TOO_LOW, &printfunc);
     REQUIRE(strcmp(printStr,"feed : 1\n") == 0);
 }
+
+TEST_CASE("Alert Controller Normal"){
+
+    alertTypeList[TO_CONTROLLER](NORMAL, &printfunc);
+    REQUIRE(strcmp(printStr,"feed : 0\n") == 0);
+}
+
+TEST_CASE("Alert Controller Too High Breach"){
+
+    alertTypeList[TO_CONTROLLER](TOO_HIGH, &printfunc);
+    REQUIRE(strcmp(printStr,"feed : 2\n") == 0);
+}
